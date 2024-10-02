d0d8da0015fd096ec26355de0295ed8f54c93fb6
RobertoSuelto <robertosuelto2014@gmail.com>
2024-10-02 18:33:24 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ echo "⟳ Starting PHP-FPM..."
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "⟳ Starting Nginx..."
+echo "✓ Preparando modulos..."
 echo "✓ Successfully started"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
